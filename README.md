141f0c69ffcac8558000a6c737a3775b71432fb5
sinri0809 <1lliodqb@gmail.com>
2022-03-09 22:37:29 +0900

    paycomap v0.0.0

---
+++ README.md
@@ -0,0 +1,19 @@
+# PAYCO 식당 여지도
+
+2022.03.09 ~
+
+> PAYCO앱이 답답햅서 만들기 시작한 아마추어 앱입니당 :)
+
+#### 파일 구조
+
+```
+
+```
+
+### React
+
+### 추가 module
+
+- AXIOS
+- ROUTER(Browser Router)
+- SCSS
--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style/__common.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>payco 식권 지도</h1>
+        <mark>수내(본사)</mark>
+        <p>총 35 개의 사용처</p>
       </header>
+
+      <main>
+        <section>
+          <h3>뭘 먹을까?</h3>
+          <ul>
+            <li>한식(밥)</li>
+            <li>한식(면)</li>
+            <li>중식</li>
+            <li>일식(초밥, 회)</li>
+            <li>일식(덮밥, 라멘)</li>
+            <li>양식</li>
+            <li>분식</li>
+          </ul>
+          <h4>어디서 먹을까?</h4>
+          <ul>
+            <li>category에 맞는 식당리스트가 나옴</li>
+          </ul>
+        </section>
+
+      </main>
+
+      <footer>
+        <p>Creator</p>
+        <dl>
+          <dt>작성자</dt>
+          <dd>정수진</dd>
+
+          <dt>부서</dt>
+          <dd>티맥스오에스/PM본부/UX팀/UX연구원</dd>
+        </dl>
+
+        <address>
+          <a href={'sinri0809.github.io'}>github</a>
+        </address>
+      </footer>
     </div>
   );
 }
